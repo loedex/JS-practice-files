9997254db6527f46ca704aa40cd70db5d41fe53f
Husnain Ahmad <loedex@yahoo.com>
2026-03-10 09:24:52 +0500

    Add 13_Math.js file with examples of Math methods and constants

---
+++ 13_Math.js
@@ -0,0 +1,65 @@
+//Math.E : returns Euler's Number
+console.log(Math.E);
+
+//Math.PI : returns pi value
+console.log(Math.PI);
+
+//Math.SQRT2  : returns the square root of 2
+console.log(Math.SQRT2);
+
+//Math.SQRT1_2 : returns the square root of 1/2
+console.log(Math.SQRT1_2);
+
+//Math.LN2 : returns the natural logarthim of 2
+console.log(Math.LN2);
+
+//Math.LN10 : returns the natural logarthim of 10
+console.log(Math.LN10);
+
+// Math Methods
+//round() : round to its nearest integer
+let num = 2.30045;
+console.log(Math.round(num));
+
+//ceil() : rounded up
+console.log(Math.ceil(num));
+
+//floor() : rounded down
+console.log(Math.floor(num));
+
+//trunc() : return the integer part
+console.log(Math.trunc(num));
+
+//sign(x) : returns if x is +ve, null or -ve
+//returns 1 if x is +ve
+//returns -1 if x is -ve
+//returns 0 if x is 0
+console.log(Math.sign(num));
+
+//Math.pow(x,y) : returns the value of x to the power of y
+console.log(Math.pow(2,2));
+
+// sqrt(x) : returns the sqrt() of x
+console.log(Math.sqrt(10));
+
+//abs(x) : returns the absolute value
+console.log(Math.abs(-3.11));
+
+//sin() 
+console.log(Math.sin(10));
+
+//cos()
+console.log(Math.cos(10));
+
+//min()
+console.log(Math.min(10,11,2,100));
+
+//max()
+console.log(Math.max(10,2,1,100,80));
+
+//Math.random()
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()));
+
+console.log(Math.floor(Math.random() * 10));
