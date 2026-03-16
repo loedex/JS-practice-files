22e68e2efd99bed9c7602cecb7792c50a74ba0ba
Husnain Ahmad <loedex@yahoo.com>
2026-03-16 22:42:16 +0500

    Add simulateButtonClick function with multiple callback examples for simulating button actions

---
+++ Excersise_02.js
@@ -0,0 +1,33 @@
+//  Simulate React button handlers using callbacks! Write a function called simulateButtonClick that takes a buttonLabel and an onClick callback. It prints "🖱️ [buttonLabel] was clicked!" then calls onClick. Create 3 different buttons:
+
+
+const simulateButtonClick = (buttonLabel,onClick)=>{
+    console.log(buttonLabel+"  was pressed");
+    onClick();
+}
+
+// Notice: onClick is just a parameter name here — it holds whatever callback function we pass in. This is identical to how React's onClick prop works internally!
+
+
+
+//Different callbacks
+const handleSignUp = ()=>{
+    console.log("Opening SignUp Form");
+};
+const deleteAccount = ()=>{
+    console.log("Deleting Account");
+};
+const viewProfile = ()=>{
+    console.log("View your profile");
+};
+
+
+//Now simulate all three button clicks
+simulateButtonClick("Sign-Up",handleSignUp);
+simulateButtonClick("Delete",deleteAccount);
+simulateButtonClick("View",deleteAccount);
+
+//inline callback
+simulateButtonClick("Logout",()=>{
+    console.log("loging you OUT");
+});
